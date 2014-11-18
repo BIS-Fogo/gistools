a2292ef5b5b446857aa43b2171e1727f8e6d519a
tnauss <nausst@gmail.com>
2014-11-18 12:43:27 +0100

    Update: Finalize field survey pre-processing

--- src/preprocessFieldCampaign2014.R
+++ src/preprocessFieldCampaign2014.R
@@ -28,13 +28,23 @@
 #  
 #  NOTE: DO NOT USE A TOP LEVEL OUTPUT PATH WHICH IS INSIDE THE TOP LEVEL INPUT
 #        PATH
-
-####Packages#########
-library(rgdal)
-
 #### User setttings ############################################################
-inpath <- "/home/alice/Desktop/kap_verde_exkursion/AGNauss_182/field_campaign_2014/raw/"
-top_level_outpath <- "/home/alice/Desktop/kap_verde_exkursion/AGNauss_182/field_campaign_2014/procd/"
+rm(list = ls(all = T))
+
+tlpath <- 
+  "/home/alice/Desktop/kap_verde_exkursion/AGNauss_182/field_campaign_2014/"
+tlpath <- "active/bis-fogo/data/field-campaign_2014/"
+
+dsn <- switch(Sys.info()[["sysname"]], 
+              "Linux" = "/media/permanent/",
+              "Windows" = "D:/")
+setwd(paste0(dsn, tlpath))
+inpath <- paste0(getwd(), "/raw/")
+outpath <- paste0(getwd(), "/procd/")
+
+library(rgdal)
+library(sp)
+library(raster)
 
 # Use this for the Cape Verdian national projection as defined by
 # SR-ORG:7391 at www.spatialreference.org and EPSG:4825 at
@@ -46,86 +56,67 @@ proj_out <- paste0("+proj=lcc +lat_1=15 +lat_2=16.66666666666667 ",
 
 #### DO NOT CHANGE ANYTHING BELOW THIS LINE EXCEPT YOU KNOW WHAT YOU DO ########
 #### Merge vegetation and animal files #########################################
-setwd(inpath)
-
-veg_tec <- read.table("plots_vegetation_2014_tec.csv", 
+veg_tec <- read.table(paste0(inpath, "plots_vegetation_2014_tec.csv"), 
                   header = FALSE, sep = ";", skip = 2, stringsAsFactors = FALSE)
-veg_agr <- read.table("plots_vegetation_2014_agr.csv", 
+veg_agr <- read.table(paste0(inpath, "plots_vegetation_2014_agr.csv"), 
                       header = FALSE, sep = ";", skip = 2, stringsAsFactors = FALSE)
-veg_nat <- read.table("plots_vegetation_2014_nat.csv", 
+veg_nat <- read.table(paste0(inpath, "plots_vegetation_2014_nat.csv"), 
                       header = FALSE, sep = ";", skip = 2, stringsAsFactors = FALSE)
-anm <- read.table("plots_animals_2014.csv", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+anm <- read.table(paste0(inpath, "plots_animals_2014.csv"), 
+                  header = TRUE, sep = ";", stringsAsFactors = FALSE)
 
 # Set column names of vegetation data frames
 colnames(veg_tec) <- sapply(veg_tec[2,], toupper)
 colnames(veg_agr) <- sapply(seq(ncol(veg_agr[2,])), function(x){
-  paste0(toupper(veg_agr[2,x]), "_", substr(veg_agr[3,x], 1, nchar(as.character(veg_agr[3,x]))-1))
+  paste0(toupper(veg_agr[2,x]), "_", 
+         substr(veg_agr[3,x], 1, nchar(as.character(veg_agr[3,x]))-1), "_AGR")
 })
 colnames(veg_nat) <- sapply(seq(ncol(veg_nat[2,])), function(x){
-  paste0(toupper(veg_nat[2,x]), "_", substr(veg_nat[3,x], 1, nchar(as.character(veg_nat[3,x]))-1))
+  paste0(toupper(veg_nat[2,x]), "_", 
+         substr(veg_nat[3,x], 1, nchar(as.character(veg_nat[3,x]))-1), "_NAT")
 })
 
 # Clean vegetation datasets
+## getting rid of line two
+veg_tec <- veg_tec[-(1:4),!(grepl("LAT|LON|ALT", colnames(veg_tec)))]
+veg_agr <- veg_agr[-(1:4),]
+veg_nat <- veg_nat[-(1:4),]
 
-## getting rid of last two lines
-veg_tec <- veg_tec[-2,]
-veg_agr <- veg_agr[-2,]
-veg_nat <- veg_nat[-2,]
 
-## AZ: correct several colnames (veg_nat) # not working yet
-colnames(veg_nat)[34] <- "GLO_5"
-colnames(veg_nat)[35] <- "GLO_10"
-colnames(veg_nat)[38] <- "HEL_5"
-colnames(veg_nat)[39] <- "HEL_10"
 
 ##AZ: replace all numeric values in the veg_nat table with "x"
-#problem: does iteration through all columns, but doesn't start at column 2 but 1
-
-#for (i in seq (2:70)){
-#  veg_nat[4:165,i][(grepl("[[:digit:]]", veg_nat[4:165,i]) & veg_nat[4:165,i] != 0)] <- "x"
-#}
-
+for(i in seq(2, ncol(veg_nat))){
+  veg_nat[,i][(grepl("[[:digit:]]", veg_nat[,i]) & veg_nat[,i] != 0)] <- "x"
+}
 
 ## merge vegetation data sets
-veg <- merge(veg_tec, veg_agr, by.x = "ID", by.y = "ID_")
-veg <- veg[c(-3,-4),]
-veg <- merge(veg, veg_nat, by.x = "ID", by.y = "ID_")
-veg <- veg[c(-2,-3),]
-veg$ID[1] <- "Info1"
-veg$ID[2] <- "Info2"
-veg$IDA[1] <- "Info1"
-veg$IDA[2] <- "Info2"
+veg <- merge(veg_tec, veg_agr, by.x = "ID", by.y = "ID__AGR")
+veg <- merge(veg, veg_nat, by.x = "ID", by.y = "ID__NAT")
 
 ##AZ: replace all values "x" with 1  #works!
 ##idea from: http://stackoverflow.com/questions/5824173/
 ##replace-a-value-in-a-data-frame-based-on-a-conditional-if-statement-in-r
-
-veg[2:163,22:125][veg[2:163,22:125]=="x"]<-1
+# veg[2:163,22:125][veg[2:163,22:125]=="x"] <- 1
 
 #AZ: replace all values "5m" with 5
-#origin of this problem ist the autofill of excel. "5m" should only be in the headlines and not 
-#in the values
-veg[2:163,22:125][veg[2:163,22:125]=="5m"]<-5
+#origin of this problem ist the autofill of excel. "5m" should only be in the 
+#headlines and not in the values
+veg[veg == "5m"] <- 5
 
-##AZ: getting rid of last line
-veg <- veg[1:164,]
-
-write.table(veg, "plots_vegetation_2014_merged.csv", sep = ",", row.names = FALSE)
+# write.table(veg, paste0(outpath, "plots_vegetation_2014_merged.csv"),
+#             sep = ",", row.names = FALSE)
 
 # Clean animal data set and merge with vegetation data set
 anm$ID <- NULL
-anm$IDA[1] <- "Info2"
-anm <- anm[c(-52, -53),]
+anm <- anm[,!(grepl("Total.*", colnames(anm)))]
 
 veg_anm <- merge(veg, anm, by = "IDA", all.x = TRUE)
 
-##AZ: getting rid of last 2 lines
-veg_anm <- veg_anm[1:162,]
-
-write.table(veg_anm, "plots_veg_anm_2014.csv", sep = ",", row.names = FALSE)
+# write.table(veg_anm, paste0(outpath, "plots_veg_anm_2014.csv"), 
+#             sep = ",", row.names = FALSE)
 
 #read GPS Data from shapefile: coordinates, elevation and Plot ID
-GPS <- readOGR("Vegplotsall.shp", layer = "Vegplotsall")
+GPS <- readOGR(paste0(inpath, "Vegplotsall.shp"), layer = "Vegplotsall")
 GPS <- data.frame(GPS[,c(1:2,5)])
 
 colnames(GPS)[4] <- "Lon"
@@ -133,7 +124,41 @@ colnames(GPS)[5] <-"Lat"
 
 veg_anm_geo <- merge(veg_anm, GPS, by.x = "ID", by.y = "name")
 
-write.table(veg_anm_geo, "plots_veg_anm_geo_2014.csv", sep = ",", row.names = FALSE)
+convert <- c("ASP", "SLP", "GLH", "BLC", "BLH", "TLC", "TLH")
+for(conv in convert){
+  veg_anm_geo[,grepl(conv, colnames(veg_anm_geo))] <-
+    as.numeric(veg_anm_geo[,grepl(conv, colnames(veg_anm_geo))])
+}
 
-##still to work on: 
-#line 81 for doesn't start to iterate columns from 3 but from 1
+veg_anm_geo$GLC[veg_anm_geo$GLC == "<5"] <- 3
+veg_anm_geo$GLC[grepl("<", veg_anm_geo$GLC)] <- 0.1
+veg_anm_geo$GLC[veg_anm_geo$GLC == "0.01"] <- 0.1
+veg_anm_geo$GLC <- as.numeric(veg_anm_geo$GLC)
+
+for(col in colnames(veg_anm_geo[,grepl("5_AGR", colnames(veg_anm_geo))])){
+  veg_anm_geo[,col] <- as.numeric(veg_anm_geo[,col])
+}
+
+veg_anm_geo[veg_anm_geo == "x" & !is.na(veg_anm_geo)] <- 1
+for(col in colnames(veg_anm_geo[,grepl("10_AGR|_NAT", colnames(veg_anm_geo))])){
+  veg_anm_geo[,col] <- as.numeric(veg_anm_geo[,col])
+}
+
+
+scol <- which(colnames(veg_anm_geo) == "RemarksAnimal")+1
+ecol <- which(colnames(veg_anm_geo) == "ele")-1
+veg_anm_geo[,scol:ecol][is.na(veg_anm_geo[,scol:ecol])] <- 0
+
+
+write.table(veg_anm_geo, paste0(outpath, "plots_veg_anm_geo_2014.csv"), 
+            sep = ",", row.names = FALSE)
+
+coordinates(veg_anm_geo) <- ~Lon + Lat
+projection(veg_anm_geo) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+str(veg_anm_geo)
+
+veg_anm_geo_lambert <- spTransform(veg_anm_geo, CRS(proj_out))
+writeOGR(veg_anm_geo_lambert, paste0(outpath, "plots_veg_anm_geo_2014_7391.shp"), 
+         "plots_veg_anm_geo_2014_7391", driver<-"ESRI Shapefile", 
+         overwrite_layer = TRUE)
+
